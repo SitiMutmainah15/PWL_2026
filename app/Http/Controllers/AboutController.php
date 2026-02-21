abeb41c070ca1aeab01ce7b37b9999e379f2e494
SitiMutmainah15 <iinsmk1rbg@gmail.com>
2026-02-22 01:03:59 +0700

    controller single

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about() {
+        return 'Nama saya Siti Mutmainah, 
+        NIM 244107020143 ';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
-use App\Http\Controllers\AboutController;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\SelamatController;
 use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -16,11 +18,11 @@ Route::get('/world', function () {
     return 'World';
 });
 
-Route::get('/', [PageController::class, 'index']);
+Route::get('/', [HomeController::class, 'home']);
 
-Route::get('/about', [PageController::class, 'about']);
+Route::get('/about', [AboutController::class, 'about']);
 
-Route::get('/articles/{id}', [PageController::class, 'articles']);
+Route::get('/articles/{id}', [ArticleController::class, 'article']);
 
 //routes name
 Route::get('/user/profile', function () {
